728e9ec468c2931d3c4d1d682c674927f46dbb8a
lauthrul <lauthrul@foxmail.com>
2021-04-11 00:42:16 +0800

    Implement functions

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# FileTimeModifier
-batch change file create/modify/access time
+# Description
+Batch change file creation/modification/access time.
+
+#Usage:
+```
+  -d string
+        specifiy directory to process
+  -f string
+        specifiy file to process
+  -r    recursive directory or not
+  -s string
+        time values to be set. 
+            @C : creation time
+            @M : modification time
+            @A : access time
+            @@ : auto choose earliest time from creation/modification/access time
+        for example:
+            @M=@C : change modification to creation time
+            @C="2020/04/10 20:00:00" : change creation time to 2020/04/10 20:00:00
+        multi-filed split with ",", for example:
+            @C=@@,@M=@@ : change creation and modification time to the earliest one
+```
+
+#For Example:
+Recursive directory `D:\Users\lauthrul\Pictures\Wallpaper` and change creation/modification/access time to the earliest one:
+<br>
+`FileTimeModifier.exe -d "D:\Users\lauthrul\Pictures\Wallpaper" -r -s "@C=@@,@M=@@,@A=@@"`
+
+Change file creation time to the earliest one under directory `D:\Users\lauthrul\Pictures\Wallpaper` (no recursive):
+<br>
+`FileTimeModifier.exe -d "D:\Users\lauthrul\Pictures\Wallpaper" -s "@C="2020/04/10 20:00:00""`
+
+Change file creation time to the earliest one:
+<br>
+`FileTimeModifier.exe -f "D:\Users\lauthrul\Pictures\Wallpaper\test.jpg" -s "@C=@@"`
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -11,84 +12,210 @@ import (
 )
 
 type param struct {
-	file   string
-	path   string
-	sets   string
-	values map[string]string
+	file      string
+	path      string
+	recursive bool
+	rawSets   string
+	sets      map[string]string
 }
 
 type fileInfo struct {
 	filePath string
-	cTime    syscall.Filetime
-	mTime    syscall.Filetime
-	aTime    syscall.Filetime
+	cTime    time.Time
+	mTime    time.Time
+	aTime    time.Time
 }
 
-func enumerateFiles(path string, files []fileInfo) error {
-	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+var (
+	keys = []string{
+		"@C", // create time
+		"@M", // modify time
+		"@A", // access time
+	}
+	values = append(keys,
+		"@@", // auto choose earliest time
+	)
+)
+
+func checkKey(s string) bool {
+	for _, k := range keys {
+		if s == k {
+			return true
+		}
+	}
+	return false
+}
+
+func checkValue(s string) bool {
+	for _, k := range values {
+		if s == k {
+			return true
+		}
+	}
+	if _, err := time.ParseInLocation("2006/01/02 15:04:05", s, time.Local); err == nil {
+		return true
+	}
+	return false
+}
+
+func enumerateFiles(root string, recursive bool, files *[]fileInfo) error {
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
+		p1, _ := filepath.Abs(path)
+		p2, _ := filepath.Abs(root)
+		if p1 == p2 {
+			return nil
+		}
+
 		if info.IsDir() {
-			return enumerateFiles(path, files)
+			if recursive {
+				return enumerateFiles(path, recursive, files)
+			} else {
+				return filepath.SkipDir
+			}
 		} else {
+			//fmt.Println(path)
 			if runtime.GOOS == "windows" {
 				stat := info.Sys().(*syscall.Win32FileAttributeData)
-				files = append(files, fileInfo{
+				*files = append(*files, fileInfo{
 					filePath: path,
-					cTime:    stat.CreationTime,
-					mTime:    stat.LastWriteTime,
-					aTime:    stat.LastAccessTime,
+					cTime:    time.Unix(0, stat.CreationTime.Nanoseconds()),
+					mTime:    time.Unix(0, stat.LastWriteTime.Nanoseconds()),
+					aTime:    time.Unix(0, stat.LastAccessTime.Nanoseconds()),
 				})
-			} /*else {
-				stat := info.Sys().(*syscall.Stat_t)
-				files = append(files, fileInfo{
-					filePath: path,
-					cTime:    stat.Ctim,
-					mTime:    stat.Mtim,
-					aTime:    stat.Atim,
-				})
-			}*/
+			} else {
+				//stat := info.Sys().(*syscall.Stat_t)
+				//files = append(files, fileInfo{
+				//	filePath: path,
+				//	cTime:    stat.Ctim,
+				//	mTime:    stat.Mtim,
+				//	aTime:    stat.Atim,
+				//})
+			}
 		}
 		return nil
 	})
 	return err
 }
 
+func setFileTime(path string, ctime, mtime, atime time.Time) (err error) {
+	path, err = syscall.FullPath(path)
+	if err != nil {
+		return
+	}
+	pathPtr, err := syscall.UTF16PtrFromString(path)
+	if err != nil {
+		return
+	}
+	handle, err := syscall.CreateFile(
+		pathPtr,
+		syscall.FILE_WRITE_ATTRIBUTES,
+		syscall.FILE_SHARE_WRITE,
+		nil,
+		syscall.OPEN_EXISTING,
+		syscall.FILE_FLAG_BACKUP_SEMANTICS,
+		0,
+	)
+	if err != nil {
+		return
+	}
+	defer syscall.Close(handle)
+	c := syscall.NsecToFiletime(syscall.TimespecToNsec(syscall.NsecToTimespec(ctime.UnixNano())))
+	m := syscall.NsecToFiletime(syscall.TimespecToNsec(syscall.NsecToTimespec(mtime.UnixNano())))
+	a := syscall.NsecToFiletime(syscall.TimespecToNsec(syscall.NsecToTimespec(atime.UnixNano())))
+	return syscall.SetFileTime(handle, &c, &a, &m)
+}
+
 func main() {
-	p := param{}
+	p := param{
+		sets: map[string]string{},
+	}
+
 	flag.StringVar(&p.file, "f", "", "指定文件")
 	flag.StringVar(&p.path, "d", "", "指定目录")
-	flag.StringVar(&p.sets, "s", "", `设置时间字段。@C:创建时间，@M:修改时间，@V:访问时间。`+
-		`如：@M=@C 表示将修改时间设置为创建时间，@C="2020/04/10 20:00:00" 表示将创建时间修改为2020/04/10 20:00:00。`+
-		`多个字段以","分开，如：@M=@C,@V=@C 表示将修改时间和访问时间都修改为创建时间`)
+	flag.BoolVar(&p.recursive, "r", false, "是否遍历子目录")
+	flag.StringVar(&p.rawSets, "s", "", `设置时间字段。@C:创建时间，@M:修改时间，@A:访问时间，@@:自动选择最早的时间。`+
+		`如：@M=@C 表示将修改时间设置为创建时间，@C="2020/04/10 20:00:00" 表示将创建时间设置为2020/04/10 20:00:00。`+
+		`多个字段以","分开，如：@M=@@,@V=@@ 表示将修改时间和访问时间都设置为（创建时间，修改时间，访问时间中）最早的时间`)
 	flag.Parse()
-	if p.file == "" || p.path == "" {
+
+	if p.file == "" && p.path == "" {
 		flag.Usage()
 		return
 	}
-	arrs := strings.Split(p.sets, ",")
-	if len(arrs) == 0 {
+
+	arrays := strings.Split(p.rawSets, ",")
+	if len(arrays) == 0 {
 		flag.Usage()
 		return
 	}
-	for _, a := range arrs {
+
+	for _, a := range arrays {
 		set := strings.Split(a, "=")
-		if len(set) != 2 {
-			continue
+		if len(set) == 2 && checkKey(set[0]) && checkValue(set[1]) {
+			p.sets[set[0]] = set[1]
 		}
-		if len(set[0]) != 2 && set[0][0] != '@' {
-			continue
+	}
+
+	if len(p.sets) == 0 {
+		flag.Usage()
+		return
+	}
+	//fmt.Printf("%+v\n", p)
+
+	var files []fileInfo
+	if err := enumerateFiles(p.path, p.recursive, &files); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	getTimeValue := func(f fileInfo, s string) time.Time {
+		switch s {
+		case "@C":
+			return f.cTime
+		case "@M":
+			return f.mTime
+		case "@A":
+			return f.aTime
+		case "@@":
+			tm := f.cTime
+			if tm.After(f.mTime) {
+				tm = f.mTime
+			}
+			if tm.After(f.aTime) {
+				tm = f.aTime
+			}
+			return tm
+		default:
+			tm, _ := time.ParseInLocation("2006/01/02 15:04:05", s, time.Local)
+			return tm
 		}
-		if len(set[1]) < 2 {
-			continue
-		}
-		if set[1][0] != '@' {
-			_, err := time.Parse("2006-01-02 15:04:05", set[1])
-			if err != nil {
-				continue
+	}
+	for _, f := range files {
+		var (
+			err error
+			c   = f.cTime
+			m   = f.mTime
+			a   = f.aTime
+		)
+		for k, v := range p.sets {
+			tm := getTimeValue(f, v)
+			switch k {
+			case "@C":
+				c = tm
+			case "@M":
+				m = tm
+			case "@A":
+				a = tm
 			}
 		}
-		p.values[set[0]] = set[1]
+		err = setFileTime(f.filePath, c, m, a)
+		result := "ok"
+		if err != nil {
+			result = err.Error()
+		}
+		fmt.Printf("%s [%s]\n\t%s\n\t%s\n\t%s\n", f.filePath, result, c, m, a)
 	}
 }
